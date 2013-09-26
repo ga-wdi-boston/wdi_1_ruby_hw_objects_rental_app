08bca9ddca9e6bc395ff1bc1ed660d1f61d858f8
Abby Howell <abby.g.howell@gmail.com>
2013-09-26 19:43:22 -0400

    initial commit

---
+++ solution/abbys_solution.rb
@@ -0,0 +1,153 @@
+class Building
+
+  attr_accessor :building_name, 
+                :address, 
+                :style, 
+                :num_of_floors, 
+                :num_of_apartments, 
+                :apartments,
+                :description
+
+  def initialize(name)
+    @building_name = name
+    randomize_address
+    randomize_style
+    @num_of_floors = (2 + rand(4))
+    @num_of_apartments = @num_of_floors * (rand(4) + 1)
+    auto_populate_apartments
+  end
+
+  def to_s
+    @description = "#{@building_name}, #{@address}, #{@num_of_apartments} apartments, built in the #{@style} style, with #{@num_of_floors} floors."
+  end
+
+  def auto_populate_apartments
+    @apartments = []
+    for x in 1..@num_of_apartments do
+    a = Apartment.new(@building_name, x)
+    @apartments.push(a)
+    end
+    return @apartments
+  end
+
+  def randomize_style
+    styles = ["Art Deco", "Colonial Revival", "Mid-Century Modern", "Gothic Revival", "Victorian"]
+    @style = styles[rand(styles.length)]
+  end
+
+  def randomize_address
+    num = 350 + rand(200)
+    streets = %w(Hancock Newbury Woodward Massachusetts Commonwealth Main)
+    endings = %w(Ave St Way)
+    @address = "#{num} #{streets[rand(streets.length)]} #{endings[rand(endings.length)]}"
+  end
+end
+
+class Apartment
+
+  attr_accessor :number,
+                :rent,
+                :square_feet,
+                :bedrooms,
+                :renters,
+                :description, 
+                :building,
+                :apartment_name,
+                :population
+  
+  def initialize(buiding, number)
+    @building = building
+    @number = number
+    @apartment_name = "#{@building} #{@number}"
+    auto_square_feet
+    auto_rent
+    auto_populate
+    @population = @renters.length
+    @description = "#{@apartment_name}, #{@square_feet} square feet, rent: $#{@rent} per month, #{@population} renters"
+  end
+
+  def to_s
+    @description
+  end
+
+  def auto_populate
+    how_many_renters = @square_feet / 300
+    @renters = []
+    how_many_renters.times do
+      a = Person.new(@apartment_name)
+      @renters.push(a)
+      end
+    return @renters
+  end
+
+  def auto_square_feet
+    @square_feet = 400 + rand(400)
+  end
+
+  def auto_rent
+    @rent = @square_feet * (2.5 + rand(1.0))
+  end
+
+  def rent_per_person
+    @rent / @renters.length
+  end
+
+  def people_in_apartment
+    @renters.each do |x|
+      puts x
+    end
+  end
+
+end
+
+class Person
+
+  attr_accessor :name, 
+                :age,
+                :gender,
+                :apartment,
+                :description
+
+  def initialize(apartment)
+    @apartment = apartment
+    randomize_name
+    randomize_gender
+    randomize_age
+  end
+
+  def randomize_name
+    first = %w(Kate John Bob Larry Ryan Mike Cori Abby Lana Rob Jon Sara Amy)
+    last = %w(McLaughlin Fischer Powell Ruddock Clement Mazzotti Tolbert Allen Golden Brendzel Cleveland)
+    @name = "#{first[rand(first.length)]} #{last[rand(last.length)]}"
+  end
+
+  def randomize_gender
+    genders = %w(male female androgynous genderqueer butch femme male female male female male female male female male female)
+    @gender = genders[rand(genders.length)]
+  end
+
+  def randomize_age
+    @age = rand(20) + rand(20) + rand(20)
+  end
+  
+  def to_s
+    @description = "#{@name}, #{@age} years old, identifies as #{@gender}, lives in #{@apartment}."
+    puts @description
+  end
+
+end
+
+b1 = Building.new("Highland Towers")
+b2 = Building.new("Binghamton Terrace")
+b3 = Building.new("Crabsbury Gardens")
+
+puts b1
+puts b1.apartments
+
+puts b2
+puts b2.apartments
+
+puts b3
+puts b3.apartments
+
+
