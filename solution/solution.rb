aa3afdbd1735a4bd97ed4711e5c3370d252fbf38
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-26 19:23:34 -0400

    Created 3 classes

---
+++ solution/solution.rb
@@ -0,0 +1,24 @@
+class Building
+	attr_accessor :address, :style, :num_floors, :apartments
+
+	def initialize
+		
+	end
+
+end
+
+class Apartment
+	attr_accessor :number, :rent, :sqft, :num_beds, :renters
+
+	def initialize
+		
+	end
+end
+
+class Person
+	attr_accessor :name, :age, :gender
+
+	def initialize
+		
+	end
+end
