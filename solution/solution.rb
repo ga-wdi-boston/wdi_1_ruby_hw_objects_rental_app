7dfb8521df5c620cb766bc49ff108fb4028c8837
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-27 07:11:21 -0400

    Creating tenants and apartments

--- solution/solution.rb
+++ solution/solution.rb
@@ -13,13 +13,31 @@ class Building
 	def to_s
 		"Apartments for rent inside!"
 	end
-#### need to split @renters 
 
 	def fill_apt
 		@apartments.each do |apartment|
 			x = apt.index[0]
+	end
+
+###### 
+	def avg_age
+		@apartments.each do |apt|
+			apartments.renters.each do |renter|
+				total_age += renter.age
+				total_renters += 1
+			end
 		end
-####
+		return (total_age / total_renters)
+
+	end
+
+	def age_total
+		@age_total = 0
+		@renters.each do |renter|
+			x = renter.age
+			@total_age += x
+		end
+		return @age_total
 	end
 
 end
@@ -27,38 +45,28 @@ end
 class Apartment
 	attr_accessor :number, :rent, :sqft, :num_beds, :renters, :each_apt
 
-	def initialize(number, rent, sqft, num_beds, renters, each_apt)
-		@renters = []
-
+	def initialize(number, rent, sqft, num_beds, renters)
+		renters = []
 		@number = number
 		@rent = rent
 		@sqft = sqft
 		@num_beds = num_beds
-		@each_apt = each_apt
 		@renters = renters
-# renters is an array of people
 	end
 
 	def to_s
 		"Check me out, I'm an apartment to live in."
 	end
 
-#goal: make a hash of roommate_groupings
-
-##split people into roommates
-	def each_apt
-		@each_apt = []
-		@renters.each do |x|
-		@each_apt <<  each_slice(3).to_a
-		end
-		return each_apt
+#rent per person if split evenly
+	def rent_each
+		return (@rent / @renters.length)
 	end
 
-#assign arrays of roommates apartments 
-	def fill_apt
-		@fill_apt = {}
-		x = @each_apt[(1...(arr.size -1)).zip @each_apt]
-		# @each_apt { |hash, each_apt| hash[each_apt] = apt_name }
+#density
+	def density
+		return (@sqft / @renters.length)
+		end
 	end
 
 end
@@ -75,31 +83,33 @@ class Person
 	def to_s
 		"I want to rent an apartment, please."
 	end
+
 end
 
-tenants = [
-Person.new('Arthur', 35, 'male'),
-Person.new('Boris', 83, 'male'),
-Person.new('Caroline', 12, 'female'),
-Person.new('Daphne', 78, 'female'),
-Person.new('Ellen', 56, 'female'),
-Person.new('Fred', 61, 'male'),
-Person.new('George', 40, 'male'),
-Person.new('Harriet', 38, 'female'),
-Person.new('Isabelle', 25, 'female'),
-Person.new('Justin', 28, 'male'),
-Person.new('Kelsey', 25, 'female'),
-Person.new('Louis', 40, 'male'),
-Person.new('Michelle', 41, 'female'),
-Person.new('Nathan', 27, 'male') ]
+# tenants- but I don't want to assign them names, because that's superfluous
+arthur = Person.new('Arthur', 35, 'male'),
+boris = Person.new('Boris', 83, 'male'),
+caroline = Person.new('Caroline', 12, 'female'),
+daphne = Person.new('Daphne', 78, 'female'),
+ellen = Person.new('Ellen', 56, 'female'),
+fred = Person.new('Fred', 61, 'male'),
+george = Person.new('George', 40, 'male'),
+harriet = Person.new('Harriet', 38, 'female'),
+isabelle = Person.new('Isabelle', 25, 'female'),
+justin = Person.new('Justin', 28, 'male'),
+kelsey = Person.new('Kelsey', 25, 'female'),
+louis = Person.new('Louis', 40, 'male'),
+michelle = Person.new('Michelle', 41, 'female'),
+nathan = Person.new('Nathan', 27, 'male') ]
 
-apartments = [
-Apartment.new(1, 1100, 1400, 2, 3),
-Apartment.new(2, 1500, 1500, 3, 3),
-Apartment.new(3, 1600, 1500, 3, 3),
-Apartment.new(4, 1800, 1800, 3, 3),
-Apartment.new(5, 1800, 2, 2) ]
+# apartments
+apartment_1 = Apartment.new(1, 1100, 1400, [arthur, boris]),
+apartment_2 = Apartment.new(2, 1500, 1500, 3, [caroline, daphne, ellen]),
+apartment_3 = Apartment.new(3, 1600, 1500, 3, [fred, george, harriet]),
+apartment_4 = Apartment.new(4, 1800, 1800, 3, [isabelle, justin, kelsey]),
+apartment_5 = Apartment.new(5, 1800, 2, [louis, michelle, nathan])
 
 
-holland_st = Building.new("118 Holland", "Victorian", 3, [1,2,3,4,5])
+holland_st = Building.new("118 Holland", "Victorian", 3, [apartment1, apartment2, apartment3, apartment4, apartment5])
 
+puts holland_st.avg_age
