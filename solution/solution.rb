104a63ffc40533ad962b805bb3accdf76851d6e3
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-26 21:47:29 -0400

    Reorganizing everything ever

--- solution/solution.rb
+++ solution/solution.rb
@@ -1,66 +1,64 @@
 class Building
 	attr_accessor :address, :style, :num_floors, :apartments
 
-	def initialize
+	def initialize(address, style, num_floors, apartments)
 		@apartments = []
 
 		@address = "address"
 		@style = "style"
 		@num_floors = num_floors
-		@apartments << Apartment.new(1, 1100, 1400, 2, 3)
-		@apartments << Apartment.new(2, 1500, 1500, 3, 3)
-		@apartments << Apartment.new(3, 1600, 1500, 3, 3)
-		@apartments << Apartment.new(4, 1800, 1800, 3, 3)
-		@apartments << Apartment.new(5, 1800, 2, 2)
+		@apartments = apartments
 	end
 
 	def to_s
 		"Apartments for rent inside!"
 	end
+#### need to split @renters 
 
 	def fill_apt
 		@apartments.each do |apartment|
 			x = apt.index[0]
 		end
-
+####
 	end
 
 end
 
 class Apartment
-	attr_accessor :number, :rent, :sqft, :num_beds, :renters
+	attr_accessor :number, :rent, :sqft, :num_beds, :renters, :each_apt
 
-	def initialize
+	def initialize(number, rent, sqft, num_beds, renters, each_apt)
 		@renters = []
 
 		@number = number
 		@rent = rent
 		@sqft = sqft
 		@num_beds = num_beds
-		@renters << Person.new('Arthur', 35, 'male')
-		@renters << Person.new('Boris', 83, 'male')
-		@renters << Person.new('Caroline', 12, 'female')
-		@renters << Person.new('Daphne', 78, 'female')
-		@renters << Person.new('Ellen', 56, 'female')
-		@renters << Person.new('Fred', 61, 'male')
-		@renters << Person.new('George', 40, 'male')
-		@renters << Person.new('Harriet', 38, 'female')
-		@renters << Person.new('Isabelle', 25, 'female')
-		@renters << Person.new('Justin', 28, 'male')
-		@renters << Person.new('Kelsey', 25, 'female')
-		@renters << Person.new('Louis', 40, 'male')
-		@renters << Person.new('Michelle', 41, 'female')
-		@renters << Person.new('Nathan', 27, 'male')
-
+		@each_apt = each_apt
+		@renters = renters
+# renters is an array of people
 	end
 
 	def to_s
 		"Check me out, I'm an apartment to live in."
 	end
 
-	def fill_apt
-		each_apt = @renters.each_slice(3).to_a
+#goal: make a hash of roommate_groupings
+
+##split people into roommates
+	def each_apt
+		@each_apt = []
+		@renters.each do |x|
+		@each_apt <<  each_slice(3).to_a
+		end
 		return each_apt
+	end
+
+#assign arrays of roommates apartments 
+	def fill_apt
+		@fill_apt = {}
+		x = @each_apt[(1...(arr.size -1)).zip @each_apt]
+		# @each_apt { |hash, each_apt| hash[each_apt] = apt_name }
 	end
 
 end
@@ -79,7 +77,29 @@ class Person
 	end
 end
 
-holland_st = Building.new
+tenants = [
+Person.new('Arthur', 35, 'male'),
+Person.new('Boris', 83, 'male'),
+Person.new('Caroline', 12, 'female'),
+Person.new('Daphne', 78, 'female'),
+Person.new('Ellen', 56, 'female'),
+Person.new('Fred', 61, 'male'),
+Person.new('George', 40, 'male'),
+Person.new('Harriet', 38, 'female'),
+Person.new('Isabelle', 25, 'female'),
+Person.new('Justin', 28, 'male'),
+Person.new('Kelsey', 25, 'female'),
+Person.new('Louis', 40, 'male'),
+Person.new('Michelle', 41, 'female'),
+Person.new('Nathan', 27, 'male') ]
 
-# ("118 Holland", "Victorian", 3, [1,2,3,4,5])
+apartments = [
+Apartment.new(1, 1100, 1400, 2, 3),
+Apartment.new(2, 1500, 1500, 3, 3),
+Apartment.new(3, 1600, 1500, 3, 3),
+Apartment.new(4, 1800, 1800, 3, 3),
+Apartment.new(5, 1800, 2, 2) ]
+
+
+holland_st = Building.new("118 Holland", "Victorian", 3, [1,2,3,4,5])
 
