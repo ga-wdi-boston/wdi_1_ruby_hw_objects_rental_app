99c597071d9485b3089746159e47f133f69bbc5d
Matt Clement <darkmoves@gmail.com>
2013-09-27 08:34:49 -0400

    Building, apt and renter arrayspopulated and callable via method.

--- solution/solution.rb
+++ solution/solution.rb
@@ -6,15 +6,30 @@ class Building
 		@style = style
 		@num_floors = num_floors
 		@apartments = apartments
+		@bhash = Hash[address: address, style: style, num_floors: num_floors, apartments: apartments]
 	end
 
 	def to_s
 		"Building: #{address}, #{style}, #{num_floors}, #{apartments}"
 	end
 
-	def apartments
-
+	def address
+		@bhash[:address]
 	end
+
+	def style
+		@bhash[:style]
+	end
+
+	def num_floors
+		@bhash[:num_floors]
+	end
+
+	def apartments
+		@bhash[:apartments]
+	end
+
+
 end
 
 class Apartment
@@ -26,15 +41,33 @@ class Apartment
 		@sqft = sqft
 		@num_beds = num_beds
 		@renters = renters
+		@ahash = Hash[number: number, rent: rent, sqft: sqft, num_beds: num_beds, renters: renters]
 	end
 
 	def to_s
 		"Apartment: #{number}, #{rent}, #{sqft}, #{num_beds}, #{renters}"
 	end
+	
+	def number
+		@ahash[:number]
+	end
+	
+	def rent
+		@ahash[:rent]
+	end
+	
+	def sqft
+		@ahash[:sqft]
+	end
+	
+	def num_beds
+		@ahash[:num_beds]
+	end
 
 	def renters
-
+		@ahash.fetch("number")
 	end
+
 end
 
 class Renter
@@ -51,26 +84,30 @@ class Renter
 		"Renter: #{name}, #{age}, #{gender}"
 	end
 
-	def hashname
+	def name
 		@rhash[:name]
 	end
 
-	def hashage
+	def age
 		@rhash[:age]
 	end
 
-	def hashgender
+	def gender
 		@rhash[:gender]
 	end
 
 end
 
-b1 = Building.new("380 Mt Auburn", "House", 2, 2)
+# Define Building 1. 
+b1 = Building.new("380 Mt Auburn", "House", 2, [1, 2])
+puts b1.apartments
 
-a11 = Apartment.new(1, 1400, 3200, 2, 2)
+# Define apartments in building 1. 
+a11 = Apartment.new(1, 1400, 3200, 2, ["George", "Anna"])
 
-a12 = Apartment.new(2, 1400, 3200, 3, 3)
+a12 = Apartment.new(2, 1400, 3200, 3, ["Erik", "Matt", "Claire"])
 
+# Define renters in building 1. 
 r111 = Renter.new("George", 50, "male")
 
 r112 = Renter.new("Anna", 40, "female")
@@ -80,10 +117,7 @@ r121 = Renter.new("Matt", 33, "male")
 r122 = Renter.new("Erik", 32, "male")
 
 r123 = Renter.new("Claire", 23, "female")
-puts r123
-puts r123.hashname
-puts r123.hashage
-puts r123.hashgender
 
-
-
+# Test hash. 
+# h = {"a" => 1, "b" => 2, "b" => 3, "b" => 4, "c" => 5, "c" => 6}
+# puts h.each_key{|b|}
