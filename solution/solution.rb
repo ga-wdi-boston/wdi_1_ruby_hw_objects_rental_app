f6bbe675d7bd9a184414276ca27616dad5d80597
Amal Hussein <amalhussein08@gmail.com>
2013-10-06 11:19:14 -0400

    Added average age method

--- solution/solution.rb
+++ solution/solution.rb
@@ -1,3 +1,4 @@
+
 class Building 
 	attr_accessor :address , :style , :num_floors , :apartments 
 
@@ -11,6 +12,19 @@ class Building
 	def to_s
 		"I'm a #{style}building on #{address} and I have #{num_floors} floors and I have a total of #{apartments} in me. Yaaay!"
 	end 
+
+	def average_age
+		counter= 0
+		sum_ages= 0
+		@apartments.each do |apartment|
+			apartment.renters.each do |renter|
+				sum_ages+=renter.age 
+				counter+=1
+			end
+		end
+		return sum_ages.to_f/ counter.to_f
+	end
+
 end
 
 class Apartment 
@@ -26,7 +40,7 @@ class Apartment
 
 	def to_s
 		"I'm apartment number #{number} and I'm #{sqft}sqft with a total of #{num_beds} bedrooms. 
-			I have #{renters} tenants living in me at the moment, and they pay #{rent} to live in me! "
+		I have #{renters} tenants living in me at the moment, and they pay #{rent} to live in me! "
 	end
 end
 
@@ -37,7 +51,6 @@ class Person
 		@name = name.to_s
 		@age = age.to_i
 		@gender = gender.to_s
-		
 	end
 
 	def to_s
@@ -46,25 +59,9 @@ class Person
 end 
 
 
-b1 = Building.new("100 main st", "brownstone" , "3", [a1,a2,a3,a4,a5,a6])
-b2 = Building.new("200 main st", "brownstone" , "3", [a7,a8,a9])
-b3 = Building.new("300 main st", "brownstone" , "3", [a10,a11])
-
-a1 = Apartment.new(101, 2000, 1800, 4, [])
-a2 = Apartment.new(102, 2000, 1800, 4, [])
-a3 = Apartment.new(202, 1800, 1500, 3, [])
-a4 = Apartment.new(203, 1800, 1500, 3, [])
-a5 = Apartment.new(204, 1700, 1400, 3, [])
-a6 = Apartment.new(205, 1500, 1200, 3, [])
-a7 = Apartment.new(301, 1200, 1000, 2, [])
-a8 = Apartment.new(101, 1100, 950, 2, [])
-a9 = Apartment.new(102, 900, 800, 1, [])
-a10 = Apartment.new(103, 850, 700, 1, [])
-a11 = Apartment.new(104, 770, 600, 1, [])
-#:name , :age , :gender 
 p1 = Person.new("Amal", 26, "woman")
-p2 = Person.new("Amal", 26, "woman")
-p3 = Person.new("Amal", 26, "woman")
+p2 = Person.new("Keyse", 35, "man")
+p3 = Person.new("Sofia", 32, "woman")
 p4 = Person.new("Amal", 26, "woman")
 p5 = Person.new("Farhiya", 29, "woman")
 p6 = Person.new("Mukhtar", 28, "man")
@@ -73,12 +70,33 @@ p8 = Person.new("Aniso", 32, "woman")
 p9 = Person.new("Shukri", 28, "woman")
 p10 = Person.new("Zahra", 24, "woman")
 p11 = Person.new("Neeneele", 29, "man")
-p12 = Person.new("Amal", 26, "woman")
-p13= Person.new("Amal", 26, "woman")
-p14= Person.new("Amal", 26, "woman")
-p15= Person.new("Amal", 26, "woman")
-p16= Person.new("Amal", 26, "woman")
-p17= Person.new("Amal", 26, "woman")
-p18= Person.new("Amal", 26, "woman")
-p19= Person.new("Amal", 26, "woman")
-p20= Person.new("Amal", 26, "woman")
+p12 = Person.new("Farah", 35, "man")
+p13 = Person.new("Sharmarke", 34, "man")
+p14 = Person.new("Fatima", 26, "woman")
+p15 = Person.new("Roda", 53, "woman")
+p16 = Person.new("Barni", 21, "woman")
+p17 = Person.new("Abdi", 14, "man")
+p18 = Person.new("Maryan", 21, "woman")
+p19 = Person.new("Anis", 32, "man")
+p20= Person.new("Abdirahman", 57, "man")
+
+
+a1 = Apartment.new(101, 2000, 1800, 4, [p1, p2, p3])
+a2 = Apartment.new(102, 2000, 1800, 4, [p5,p6,p7])
+a3 = Apartment.new(202, 1800, 1500, 3, [p9])
+a4 = Apartment.new(203, 1800, 1500, 3, [p11,p12])
+a5 = Apartment.new(204, 1700, 1400, 3, [p13,p14])
+a6 = Apartment.new(205, 1500, 1200, 3, [p15,p16])
+a7 = Apartment.new(301, 1200, 1000, 2, [p17,p18])
+a8 = Apartment.new(101, 1100, 950, 2, [p19,p20])
+a9 = Apartment.new(102, 900, 800, 1, [p4])
+a10 = Apartment.new(103, 850, 700, 1, [p8])
+a11 = Apartment.new(104, 770, 600, 1, [p10])
+
+
+b1 = Building.new("100 main st", "modern" , "3", [a1,a2,a3,a4,a5,a6])
+b2 = Building.new("200 main st", "brownstone" , "3", [a7,a8,a9])
+b3 = Building.new("300 main st", "brownstone" , "3", [a10,a11])
+
+
+puts "#{b1.address} and age is #{b1.average_age}"
